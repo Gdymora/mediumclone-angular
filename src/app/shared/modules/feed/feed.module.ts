b12589a5cb259629ddf1cc1633b42ef7f0676003
unknown <alekspetrofint@gmail.com>
2021-05-12 18:52:05 +0300

    loading, error message module

--- src/app/shared/modules/feed/feed.module.ts
+++ src/app/shared/modules/feed/feed.module.ts
@@ -8,12 +8,17 @@ import {GetFeedEffect} from 'src/app/shared/modules/feed/store/effects/getFeed.e
 import {StoreModule} from '@ngrx/store'
 import {reducers} from 'src/app/shared/modules/feed/store/reducers'
 
+import {ErrorMessageModule} from 'src/app/shared/modules/errorMessage/errorMessage.module'
+import {LoadingModule} from 'src/app/shared/modules/loading/loading.module'
+
 @NgModule({
   imports: [
     CommonModule,
     EffectsModule.forFeature([GetFeedEffect]),
     StoreModule.forFeature('feed', reducers),
-    RouterModule
+    RouterModule,
+    ErrorMessageModule,
+    LoadingModule
   ],
   declarations: [FeedComponent],
   exports: [FeedComponent],
