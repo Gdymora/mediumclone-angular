fc2ac281469778164f38fb8cb0a7bbeaef2302b0
Alexandr <alekspetrofint@gmail.com>
2021-05-12 13:52:33 +0300

    Middleware

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
-import {BrowserModule} from '@angular/platform-browser'
-import {NgModule} from '@angular/core'
-import {StoreModule} from '@ngrx/store'
-import {StoreDevtoolsModule} from '@ngrx/store-devtools'
-import {HttpClientModule} from '@angular/common/http'
-import {EffectsModule} from '@ngrx/effects'
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
+import { StoreModule } from '@ngrx/store'
+import { StoreDevtoolsModule } from '@ngrx/store-devtools'
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
+import { EffectsModule } from '@ngrx/effects'
 
-import {AppRoutingModule} from './app-routing.module'
-import {TopBarModule} from 'src/app/shared/modules/topBar/topBar.module'
-import {AppComponent} from './app.component'
-import {AuthModule} from 'src/app/auth/auth.module'
-import {environment} from 'src/environments/environment'
+import { AppRoutingModule } from './app-routing.module'
+import { TopBarModule } from 'src/app/shared/modules/topBar/topBar.module'
+import { AppComponent } from './app.component'
+import { AuthModule } from 'src/app/auth/auth.module'
+import { environment } from 'src/environments/environment'
+import { PersistanceService } from './shared/services/persistance.service'
+import { AuthInterceptor } from 'src/app/shared/services/authinterceptor.service'
 
 
 @NgModule({
@@ -27,7 +29,12 @@ import {environment} from 'src/environments/environment'
     EffectsModule.forRoot([]),
     TopBarModule
   ],
-  providers: [],
+  providers: [PersistanceService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
